e4b9f61b32aa366f7e67721998392638157bcd0c
Tim Baek <tim.baek@hotmail.com>
2017-06-12 01:06:00 -0400

    A2a: debugging

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -115,6 +115,9 @@ syscall(struct trapframe *tf)
 			  (int)tf->tf_a2,
 			  (int *)(&retval));
 	  break;
+	case SYS_fork:
+		err = sys_fork(tf,(pid_t *)&retval);
+		break;
 	case SYS__exit:
 	  sys__exit((int)tf->tf_a0);
 	  /* sys__exit does not return, execution should not get here */
@@ -132,11 +135,6 @@ syscall(struct trapframe *tf)
 #endif // UW
 
 	    /* Add stuff here */
- #if OPT_A2
-	  case SYS_fork:
-	  	err = sys_fork(tf,(pid_t *)&retval);
-	  	break;
- #endif
 
 	default:
 	  kprintf("Unknown syscall %d\n", callno);
